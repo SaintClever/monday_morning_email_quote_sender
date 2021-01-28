ef699585c75be5f4a43ebdd5f7caba360b939e2a
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-28 15:55:10 -0500

    port_number update

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import datetime, smtplib, random
 
 
+
 # Datetime
 now = datetime.datetime.now()
 weekday = now.weekday()
 # print(type(weekday)) # 0 = Mon, 1 = Tue, 2 = Wed, 3 = Thr, 4 = Fri, 5 = Sat, 6 = Sun
+
 
 
 # Email
@@ -19,6 +21,7 @@ password = 'my_password' # or input('password: ')
 # Outlook: outlook.office365.com
 # Yahoo: smtp.mail.yahoo.com
 smtp_server = 'smtp.gmail.com'
+port_number = 587 # 465
 
 
 
@@ -34,20 +37,20 @@ if weekday == 0:
         
         
     try:        
-        with smtplib.SMTP(smtp_server, port=587) as connection:
+        with smtplib.SMTP(smtp_server, port=port_number) as connection:
             connection.ehlo()
             connection.starttls()
             connection.login(user=sender_email, password=password)
             connection.sendmail(
                 from_addr=sender_email,
                 to_addrs=recipient_email,
-                msg= ' '.join([
+                msg=' '.join([
                     f'From: {sender_email}',
                     f'To: {recipient_email}',
                     f'Subject: Monday morning positivity quote',
                     '',
                     f'{quote}'
-                ]).encode('utf-8').strip()
+                ]).encode('utf-8').strip() # may not need .encode('utf-8').strip()
             )
     except Exception as exception:
         print(f'Error: {exception}')
